2afa3ab6532e012050838ce60a82a7225fd4f827
minexew <minexew@gmail.com>
2020-04-16 15:27:04 +0000

    WIP statically-linked kernel

--- host.c
+++ host.c
@@ -7,24 +7,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-__attribute__((used)) static uint32_t HostGetApiVersion(void) {
+uint32_t HostGetApiVersion(void) {
     // 0xMMMMmmmm
     // MAJOR = breaking changes
     // minor = backwards-compatible changes
     return 0x00030000;
 }
 
-__attribute__((used)) static const char* HostGetEnv(const char* name) {
+const char* HostGetEnv(const char* name) {
     return getenv(name);
 }
 
-__attribute__((used)) static void HostGetLocalTime(struct CDateStruct* ds_out) {
+void HostGetLocalTime(struct CDateStruct* ds_out) {
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
     timespec_to_CDateStruct_local(ts.tv_sec, ts.tv_nsec, ds_out);
 }
 
 // Generate thunks for translating TempleOS ABI to SysV
+// (only needed for dynamic loading)
 
 MAKE_THUNK0(HostGetApiVersion)
 MAKE_THUNK1(HostGetEnv)
